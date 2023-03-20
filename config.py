c3c25387b8817df7390f0bc8a24d82e0bc4f7ea3
Vchslv-Zkhrv <vchslv.zkhrv@gmail.com>
2023-03-20 19:38:00 +0300

    renamed script -> application

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 import os
 import sys
+from dataclasses import dataclass
+from abc import abstractmethod
+from typing import Literal, TypedDict, NewType
+from datetime import datetime
+
+from PyQt6 import QtWidgets, QtGui, QtCore, QtSvg
+from loguru import logger
+
