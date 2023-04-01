dc190856b7c0a6eec7935f8c5913d145f86c8ed2
Vchslv-Zkhrv <vchslv.zkhrv@gmail.com>
2023-04-01 19:56:12 +0300

    changed requirements

--- app/application.py
+++ app/application.py
@@ -1,8 +1,11 @@
 import sys
+from pprint import pprint
+import inspect
 
 from loguru import logger
-
 from PyQt6 import QtWidgets
+import pyqt_custom_titlebar_window as ctw
+
 from .qwindows import Window
 
 """
@@ -18,6 +21,7 @@ class Application(QtWidgets.QApplication):
     def __init__(self):
         QtWidgets.QApplication.__init__(self, sys.argv)
         self.window = Window()
+        ctw.customTitlebarWindow.FramelessWindow()
         self.window.show()
         logger.debug("all ready to start")
         sys.exit(self.exec())
